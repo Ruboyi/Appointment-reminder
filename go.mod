2717a6b238aacc32c1f41adec9f077726f33f8bd
Gradhoc - rlopez <108720344+rlopezcofrico@users.noreply.github.com>
2024-02-29 11:43:35 +0100

    UPDATE reminder GO

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module my_sql_test
 go 1.22.0
 
 require github.com/go-sql-driver/mysql v1.7.1
+
+require (
+	github.com/sendgrid/rest v2.6.9+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.14.0+incompatible // indirect
+)
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func sendEmail(toEmail string, name string, appointmentDate string) error {
+    from := mail.NewEmail("Atkinson Barber Shop", "Atkinsonbarbershop@gmail.com")
+    subject := "Recordatorio de Cita: ¡Te Esperamos Pronto!"
+    to := mail.NewEmail("Estimado/a "+name, toEmail)
+
+    appointmentDateTime, err := time.Parse("2006-01-02 15:04:05", appointmentDate)
+    if err != nil {
+        return err
+    }
+    appointmentTime := appointmentDateTime.Format("15:04")
+
+    htmlContent := fmt.Sprintf(`<div style="text-align: center;">
+    <img src="https://atkinsonbarbershop.com/wp-content/uploads/2017/06/logoatkinsonheader.png" alt="Logo Atkinson Barber Shop" style="width: 200px; height: auto; margin: 20px auto;">
+    <h1>Atkinson Barber Shop</h1>
+    <p>Hola %s,</p>
+    <p>Te recordamos que tienes una cita programada con nosotros para mañana:</p>
+    <p>Hora de la cita: %s</p>
+    <p>Por favor, asegúrate de llegar a tiempo. Estamos ansiosos por verte.</p>
+    <p>Si necesitas reprogramar o cancelar tu cita puedes hacerlo a través de nuestra App, por favor contáctanos con anticipación.</p>
+    <p>¡Gracias y nos vemos pronto en Atkinson Barber Shop!</p>
+  </div>`, name, appointmentTime)
+
+    message := mail.NewSingleEmail(from, subject, to, "", htmlContent)
+    client := sendgrid.NewSendClient("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")
+    response, err := client.Send(message)
+    if err != nil {
+        return err
+    }
+    fmt.Println(response.StatusCode)
+    fmt.Println(response.Body)
+    fmt.Println(response.Headers)
+    return nil
+}
+
+
+func main() {
+    for {
+        now := time.Now()
+        targetTime := time.Date(now.Year(), now.Month(), now.Day(), 21, 00, 0, 0, now.Location())
+
+        if now.After(targetTime) {
+            targetTime = targetTime.Add(24 * time.Hour)
+        }
+        duracion := targetTime.Sub(now)
+
+
+        time.Sleep(duracion)
+
+        db, err := sql.Open("mysql", "root:h1EFA3CDf246eh642fabhBDBgChEEfGH@tcp(viaduct.proxy.rlwy.net:41346)/railway")
+        if err != nil {
+            panic(err)
+        }
+        now = time.Now()
+        initDayBefore := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location())
+        endDayBefore := time.Date(now.Year(), now.Month(), now.Day()+1, 23, 59, 59, 0, now.Location())
+
+       
+        rows, err := db.Query("SELECT appointments.idUser, appointments.appointmentDate, users.nameUser, users.email FROM appointments INNER JOIN users ON appointments.idUser = users.idUser WHERE appointments.appointmentDate BETWEEN ? AND ? AND users.email IS NOT NULL AND appointments.canceled != 1", initDayBefore.Format("2006-01-02 15:04:05"), endDayBefore.Format("2006-01-02 15:04:05"))
+        if err != nil {
+            panic(err)
+        }
+
+
+        for rows.Next() {
+            var idUser int
+            var appointmentDate string
+            var name string
+            var email string
+           
+            if err := rows.Scan(&idUser, &appointmentDate, &name, &email); err != nil {
+                panic(err)
+            }
+            fmt.Println(idUser , appointmentDate, name , email)
+            sendEmail(email, name, appointmentDate)
+        }
+        
+  
+        db.Close()
+        if err := rows.Err(); err != nil {
+            panic(err)
+        }
+    }
+}
