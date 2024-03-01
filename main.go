f3d634d96ed9b0258dd9047fca6ea75feedfdded
Gradhoc - rlopez <108720344+rlopezcofrico@users.noreply.github.com>
2024-03-01 11:57:08 +0100

    Update main.go

--- main.go
+++ main.go
@@ -71,7 +71,9 @@ func sendEmail(toEmail, name, appointmentDate string) error {
 
 func main() {
 	fmt.Println("Iniciando el servicio de recordatorio de citas...")
+    fmt.Println("Hora objetivo: ", targetHour + ":" + targetMinute)
 	for {
+        fmt.Println("Esperando a la hora objetivo...")
 		now := time.Now().In(time.FixedZone(timezone, getTimezoneOffset(timezone)))
 		
         targetTimeParts := []int{0, 0}
@@ -79,18 +81,22 @@ func main() {
         fmt.Sscanf(targetMinute, "%d", &targetTimeParts[1])
 
         targetTime := time.Date(now.Year(), now.Month(), now.Day(), targetTimeParts[0], targetTimeParts[1], 0, 0, now.Location())
-
+        fmt.Println("Target time: ", targetTime)
 		if now.After(targetTime) {
 			targetTime = targetTime.Add(24 * time.Hour)
 		}
 		duration := targetTime.Sub(now)
 
+        fmt.Println("Esperando ", duration)
 		time.Sleep(duration)
 
+        fmt.Println("Hora de enviar correos")
+        fmt.Println("Conectando a la base de datos...")
 		db, err := sql.Open("mysql", sqlAlchemyURI)
 		if err != nil {
 			panic(err)
 		}
+        fmt.Println("Conexión exitosa")
 		now = time.Now().In(time.FixedZone(timezone, getTimezoneOffset(timezone)))
 		initDayBefore := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location())
 		endDayBefore := time.Date(now.Year(), now.Month(), now.Day()+1, 23, 59, 59, 0, now.Location())
@@ -115,11 +121,16 @@ func main() {
 			time.Sleep(10 * time.Second)
 		}
 
+        rows.Close()
+
 		db.Close()
 		if err := rows.Err(); err != nil {
 			panic(err)
 		}
+
+        fmt.Println("Correos enviados")
 	}
+
 }
 
 func getTimezoneOffset(zone string) int {
